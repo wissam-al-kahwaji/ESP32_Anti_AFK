9a777aa9b2afd35df7c6aa6ff2ea378179070f11
Wissam Al Kahwaji <wissamalkahwaji@gmail.com>
2026-09-12 17:17:05 +0300

    Create ESP32_Anti_AFK.ino

---
+++ ESP32_Anti_AFK.ino
@@ -0,0 +1,59 @@
+#include <USB.h>
+
+#include <USBHIDKeyboard.h>
+
+USBHIDKeyboard Keyboard;
+
+
+
+char keys[] = {'w', 's', 'a', 'd', ' '};
+
+int totalKeys = 5;
+
+
+
+void setup() {
+
+USB.begin();
+
+Keyboard.begin();
+
+
+
+randomSeed(analogRead(0));
+
+}
+
+
+
+void loop() {
+
+int randomIdx = random(0, totalKeys);
+
+char selectedKey = keys[randomIdx];
+
+
+
+Keyboard.press(selectedKey);
+
+
+
+delay(random(150, 400));
+
+
+
+Keyboard.releaseAll();
+
+
+
+if (selectedKey == ' ') {
+
+delay(500);
+
+}
+
+
+
+long randomDelay = random(5500, 6500);
+
+delay(randomDelay);
